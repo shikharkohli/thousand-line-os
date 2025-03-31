3ee4e7eed9b735f2cbb4af39e5ce0cf7997405cd
Shikhar K <shikharkohli@gmail.com>
2025-03-31 16:54:05 +0530

    kernel code and run script now updated to launch barebones OS kernel

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
-#!bin/bash
+#!/bin/bash
 set -xue
 
-#QEMU file path
+# QEMU file path
 QEMU=/opt/homebrew/bin/qemu-system-riscv32
 
-#Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+# Path to clang and compiler flags
+CC=/opt/homebrew/opt/llvm/bin/clang  
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
+# Start QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
